4836b5551e7a46d064c6539720ebf68fc2778a06
patelmuskan09 <patelmuskan672@gmail.com>
2025-11-07 13:43:12 +0530

    Add files via upload

---
+++ equalstring.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class equalstring {
+    public static void main(String[] args) {
+        String str="hello";
+        String str1="hello";
+        char ch[]=str.toCharArray();
+        char ch1[]=str1.toCharArray();
+        Arrays.sort(ch);
+        Arrays.sort(ch1);
+        String val1=new String(ch);
+        String val2=new String(ch1);
+        if(val1.equals(val2)){
+            System.out.println("TRUE");
+        }else{
+            System.out.println("FALSE");
+        }
+    }
+}
