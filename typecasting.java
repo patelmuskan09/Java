876dde44a9bbe9bec52b48f4286be677ffa80ebc
patelmuskan09 <patelmuskan672@gmail.com>
2025-11-07 12:49:12 +0530

    Add files via upload

---
+++ typecasting.java
@@ -0,0 +1,9 @@
+public class typecasting {
+    public static void main(String[] args) {
+        float num = 7.89f;
+        int intNum = (int) num; // type casting float → int
+
+        System.out.println("Float value: " + num);
+        System.out.println("Integer value after casting: " + intNum);
+    }
+}
