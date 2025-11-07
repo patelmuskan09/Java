1e6afd764dfba5fa9d4bbdbd645c366cbec6312c
patelmuskan09 <patelmuskan672@gmail.com>
2025-11-07 13:20:48 +0530

    Add files via upload

---
+++ table.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class table {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n = sc.nextInt();
+        int i=1;
+        while (i<=10){
+            System.out.println(n + " x " + i + " = " + (n * i));
+            i=i+1;
+        }
+    }
+}
