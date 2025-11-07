f0d12d1c9478d4ffdd75d93850fcf650d6eb6039
patelmuskan09 <patelmuskan672@gmail.com>
2025-11-07 12:59:02 +0530

    Add files via upload

---
+++ print.java
@@ -0,0 +1,8 @@
+public class print {
+    public static void main(String[] args){
+        String str="hello";
+        for(int i=0;i<str.length();i++){
+            System.out.println(str.charAt(i));
+        }
+    }
+}
