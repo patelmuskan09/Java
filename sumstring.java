bdbd6263a40ed5b4ea36dfb3043b62e8fc5106a2
patelmuskan09 <patelmuskan672@gmail.com>
2025-11-11 15:17:45 +0530

    Add files via upload

---
+++ sumstring.java
@@ -0,0 +1,9 @@
+public class sumstring {
+     static void sum() {
+        System.out.println("MUSKAANNNNNNNNNNNNNNNNNNNNNNN");
+    }
+    public static void main(String[] args) {
+         sumstring obj= new sumstring();
+              obj.sum();
+    }
+}
