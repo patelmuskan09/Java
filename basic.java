6256652bf72a59d2044e1f6207a868d5070ca68e
patelmuskan09 <patelmuskan672@gmail.com>
2025-11-13 10:08:38 +0530

    Add files via upload

---
+++ basic.java
@@ -0,0 +1,10 @@
+public class basic {
+    basic(int a){
+        System.out.println("heyooo");
+    }
+}
+class k{
+    public static void main(String[] args) {
+        basic s1 = new basic(10);
+    }
+}
