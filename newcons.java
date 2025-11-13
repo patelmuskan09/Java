5793b5bf12417b6a470ec380d1e964a33782d3e4
patelmuskan09 <patelmuskan672@gmail.com>
2025-11-13 15:26:58 +0530

    Add files via upload

---
+++ newcons.java
@@ -0,0 +1,22 @@
+public class Newcons {
+    public Newcons() {
+        System.out.println("MUSKAANNNNNNNNN");
+    }
+
+    public Newcons(int a) {
+        System.out.println("value of a: "+a);
+    }
+ public Newcons(int a, int b) {
+        System.out.println("value of a: "+ a + " ,value of b: " +b);
+    }
+
+
+        public static void main(String[] args) {
+             Newcons s1=new Newcons();
+            Newcons s2 = new Newcons(15);
+            Newcons s3=new Newcons(20,30);
+            System.out.println();
+        }
+    }
+
+
