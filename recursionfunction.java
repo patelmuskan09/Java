f4298a5012bf318ab694c6d91270eb05e8e7cd7a
patelmuskan09 <patelmuskan672@gmail.com>
2026-04-13 10:56:33 +0530

    Add files via upload

---
+++ recursionfunction.java
@@ -0,0 +1,11 @@
+public class recursionfunction {
+    static int gcd(int a,int b)
+    {
+        if(b==0) return a;
+        return gcd(b, a%b);
+    }
+    public static void main(String[] args){
+        int a=20, b=30;
+        System.out.println(gcd(a,b));
+    }
+}
