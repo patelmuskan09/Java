8891cabaeef118d0c1936ce30227e75ef3348979
patelmuskan09 <patelmuskan672@gmail.com>
2026-04-28 11:49:19 +0530

    Add files via upload

---
+++ literals.java
@@ -0,0 +1,11 @@
+public class literals {
+     public static void main(String args[]){
+        int num1=0b101;
+        System.out.println(num1);
+        char c='a';
+        System.out.println(c);
+        char c1='b';
+        c++;
+        System.out.println(c1);
+     }
+}
