e653c6ac81cd3239891cd4cdf7513e67d1a48d20
patelmuskan09 <patelmuskan672@gmail.com>
2026-04-28 11:47:29 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,14 @@
+public class datatype {
+     public static void main(String args[]){
+        int num1=9;
+        byte b= 127;
+        short s=120;
+        long l=1293647l;
+        float f=3.14f;
+        double d=12.3;
+        char c='k' ;
+        boolean b1=true;
+      }
+
+}
+
