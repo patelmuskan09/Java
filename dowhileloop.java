9e1cdb5ca2fb060f6cf173517d906ef7c39fe722
patelmuskan09 <patelmuskan672@gmail.com>
2026-04-28 11:48:02 +0530

    Add files via upload

---
+++ dowhileloop.java
@@ -0,0 +1,11 @@
+public class dowhileloop {
+    public static void main(String[] args) {
+        int i=1;
+        do{
+            System.out.println("SHUSSHSHSHSHSHSHSSSS");
+            i++;
+        }
+        while(i<=21);
+        
+    }
+}
