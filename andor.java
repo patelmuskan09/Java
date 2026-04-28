3c14a7ccbeef8375146348a3b1df8b1f5224a75b
patelmuskan09 <patelmuskan672@gmail.com>
2026-04-28 11:36:45 +0530

    Add files via upload

---
+++ andor.java
@@ -0,0 +1,13 @@
+public class andor {
+    public static void main(String[] args){
+      int a = 30;
+      int b = 20;
+      int x=9;
+      int y=21;
+      boolean r = a>b && x<y;
+      System.out.println(r);
+      boolean r2 = a<b || x>y;
+      System.out.println(r2);
+   }
+    }
+
