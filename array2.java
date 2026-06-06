8c3ae3adbf9dcf3d3702ca9b3f506315d99c56c6
patelmuskan09 <patelmuskan672@gmail.com>
2026-06-06 20:00:13 +0530

    Add files via upload

---
+++ array2.java
@@ -0,0 +1,16 @@
+public class array2 {
+    public static void main(String a[]){
+        int nums[]=new int[4];
+        nums[0]=4;
+        nums[1]=7;
+        nums[2]=6;
+        nums[3]=8;
+        System.out.println(nums[1]);
+
+
+        for(int i=0;i<4;i++){
+            System.out.println(nums[i]);
+        }
+
+    }
+}
